a2a7a0ca7fc9014cfc493e971b9f4460e6916f2a
anagabrieleloor <anagabrieleloor@outlook.com>
2023-08-18 15:38:48 -0400

    components tolder structure

---
+++ src/components/Delete.jsx
@@ -0,0 +1,11 @@
+
+
+
+
+export default function Delete() {
+    return (
+        <div>
+            
+        </div>
+    )
+    }
---
+++ src/components/Search.jsx
@@ -0,0 +1,12 @@
+
+
+
+
+
+export default function Search() {
+    return (
+        <div>
+            
+        </div>
+    )
+    }
